9545ef57c581e63f225a26e39bb1f4a3e7a67b93
Catalina Riquelme <riquelmecata@gmail.com>
2023-09-11 23:32:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import ProductManager from "./controllers/ProductManager.js"
+
+const product = new ProductManager();
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get("/products", async (req, res) => {
+    res.send(await product.getProducts())
+})
+
+app.get("/products/:id", async (req, res) => {
+    let id = req.params.id
+    res.send(await product.getProductsById(id))
+})
+
+app.post("/products", async (req, res) => {
+    let newProduct = req.body
+    res.send(await product.addProducts(newProduct))
+})
+
+app.put("/products/:id", async (req, res) => {
+    let id = req.params.id
+    let updateProducts = req.body;
+    res.send(await product.updateProducts(id, updateProducts))
+})
+
+app.delete("/products/:id", async (req, res) => {
+    let id = req.params.id
+    res.send(await product.deleteProducts(id))
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express Puerto ${PORT}`)
+})
